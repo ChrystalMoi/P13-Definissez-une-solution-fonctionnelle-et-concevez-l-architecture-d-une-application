c191e80d36c598344b2affe732093e6ef1dfe1fe
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-12-06 10:58:08 +0100

    Initialisation du projet Angular pour le PoC

---
+++ src/app/talkjs-chat/talkjs-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-talkjs-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './talkjs-chat.component.html',
+  styleUrl: './talkjs-chat.component.css'
+})
+export class TalkjsChatComponent {
+
+}
