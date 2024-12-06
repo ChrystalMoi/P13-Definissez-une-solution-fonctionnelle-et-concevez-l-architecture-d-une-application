c3abb9486810ac33932188d55db546a3bf0674d5
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-12-06 15:19:21 +0100

    PoC du projet 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { TalkjsChatComponent } from './talkjs-chat/talkjs-chat.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TalkjsChatComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'poc-chat';
+
+  @ViewChild(TalkjsChatComponent) chat!: TalkjsChatComponent;
+
+  constructor(private router: Router) {}
+
+  onToggleChanged(event: Event) {
+    const isChecked = (<HTMLInputElement>event.target).checked;
+    this.chat.isHelper = isChecked;
+    this.chat.refreshChatSession();
+  }
 }
--- src/app/talkjs-chat/talkjs-chat.component.ts
+++ src/app/talkjs-chat/talkjs-chat.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import Talk from 'talkjs';
 
 @Component({
   selector: 'app-talkjs-chat',
   standalone: true,
   imports: [],
-  templateUrl: './talkjs-chat.component.html',
-  styleUrl: './talkjs-chat.component.css'
+  template: '',
+  styles: '',
 })
-export class TalkjsChatComponent {
+export class TalkjsChatComponent implements OnInit, OnDestroy {
+  public isHelper: boolean = false;
+  private session!: Talk.Session;
 
+  ngOnInit(): void {
+    this.initChatSession();
+  }
+  ngOnDestroy(): void {
+    this.session?.destroy();
+  }
+
+  initChatSession(): void {
+    Talk.ready.then((): void => {
+      const user = new Talk.User({
+        id: 'user',
+        name: 'User',
+        email: 'user@example.com',
+        photoUrl: 'https://cdn-icons-png.flaticon.com/512/6596/6596121.png',
+        role: 'default',
+        welcomeMessage: null,
+      });
+
+      const helper = new Talk.User({
+        id: 'helper',
+        name: 'Chrystal',
+        email: 'helper@example.com',
+        photoUrl: 'https://talkjs.com/new-web/avatar-1.jpg',
+        welcomeMessage:
+          'Bonjour !\nJe suis Chrystal du support client, comment puis-je vous aider ?',
+        role: 'default',
+      });
+
+      this.session = new Talk.Session({
+        appId: 'tMiEuz6x',
+        me: this.isHelper ? helper : user,
+      });
+
+      const conversation = this.session.getOrCreateConversation(
+        'nouvelle_conversation'
+      );
+      conversation.setParticipant(user);
+      conversation.setParticipant(helper);
+
+      const popup = this.session.createPopup();
+      popup.select(conversation);
+      popup.mount({ show: true });
+    });
+  }
+
+  refreshChatSession(): void {
+    this.session?.destroy();
+    this.initChatSession();
+  }
 }
